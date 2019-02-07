658e4603d1215d0217258a23ab84e45da7569593
Lisa Lemons - Grand Circus <LisaLemonsGrandCircus@Katies-MacBook-Air.local>
2019-02-06 20:48:36 -0500

    did some wild stuff

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hello, world!');
+"use strict";
+
+let hello = "Hello";
+let world = 'a';
+// console.log(`${hello}, ${world}!`);
+// console.log('Hello, world!');
+
+let startup = "";
+console.log(startup);
+
+if (startup) {
+    console.log(`${hello}, ${startup}`);
+} 
+else if (world.length > 0) {
+    console.log(`${hello}, ${world}`);
+} else {
+    console.log(`${hello}, ${world} 2`);
+}
+
+let x = prompt('what is your name?');
+console.log(x);
